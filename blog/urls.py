2abe835d0e5ced647b80d3f4fdbbb12aef91419c
Redjep Mollaali <mollaali98@gmail.com>
2020-06-01 21:36:51 +0300

    add users

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
     path('', view=views.home, name='blog-home'),
     path('about/', view=views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm
 from .models import Post
+from django.contrib import messages
+
+
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'blog/register.html', context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('blog-home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'blog/login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/blog/')
 
 
 def home(request):
