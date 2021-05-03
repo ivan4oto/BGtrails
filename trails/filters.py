dce4e0412263437b8179036823054f025949823c
Ivan <redial@abv.bg>
2021-05-03 13:29:41 +0300

    django filters refactor

--- trails/filters.py
+++ trails/filters.py
@@ -1,13 +1,18 @@
 import django_filters
+from django import forms
 
 from .models import Trail
 
 
 class TrailFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains', widget=forms.TextInput(attrs={'class': "form-control"}))
+
+    distance_lt = django_filters.NumberFilter(field_name='distance', lookup_expr='lt', widget=forms.NumberInput(attrs={'class': "form-control"}))
+    distance_gt = django_filters.NumberFilter(field_name='distance', lookup_expr='gt', widget=forms.NumberInput(attrs={'class': "form-control"}))
+
+    elevation_lt = django_filters.NumberFilter(field_name='elevation', lookup_expr='lt', widget=forms.NumberInput(attrs={'class': "form-control"}))
+    elevation_gt = django_filters.NumberFilter(field_name='elevation', lookup_expr='gt', widget=forms.NumberInput(attrs={'class': "form-control"}))
+
     class Meta: 
         model = Trail
-        fields = {
-            'name': ['exact'],
-            'distance': ['lt', 'gt'],
-            'elevation': ['lt', 'gt']
-        }
+        fields = ('name', 'distance', 'elevation')
